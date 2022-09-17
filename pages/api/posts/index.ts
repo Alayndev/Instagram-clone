ed8e2e48ee0737b37712928004d3815c69fb1aff
AlaynDev <saezalayn@gmail.com>
2022-09-17 19:19:37 -0300

    URL fixed

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -17,6 +17,7 @@ let bodySchema = yup
 
 async function getPosts(req: NextApiRequest, res: NextApiResponse<PostType[]>) {
   const posts: PostType[] = await getAllPosts();
+  console.log("🚀 ~ file: index.ts ~ line 20 ~ getPosts ~ posts", posts);
 
   res.status(200).json(posts);
 }
